b1c8b40139d03f37f8cc2eb20ab5948310f0a55e
Franckyi <franck.velasco@hotmail.fr>
2021-07-22 17:14:30 +0200

    feat: added 'Dyed' hide flag

--- src/main/java/com/github/franckyi/ibeeditor/client/gui/editor/item/HideFlagsCategory.java
+++ src/main/java/com/github/franckyi/ibeeditor/client/gui/editor/item/HideFlagsCategory.java
@@ -14,17 +14,19 @@ public class HideFlagsCategory extends AbstractCategory {
         this.hideFlags = itemStack.getOrCreateTag().getInt("HideFlags");
         this.addAll(
                 new PropertyBoolean("Hide Enchantments",
-                        this.hasHideFlag(5), b -> this.addHideFlags(b ? 1 : 0)),
+                        this.hasHideFlag(6), b -> this.addHideFlags(b ? 1 : 0)),
                 new PropertyBoolean("Hide Attribute modifiers",
-                        this.hasHideFlag(4), b -> this.addHideFlags(b ? 2 : 0)),
+                        this.hasHideFlag(5), b -> this.addHideFlags(b ? 2 : 0)),
                 new PropertyBoolean("Hide Unbreakable",
-                        this.hasHideFlag(3), b -> this.addHideFlags(b ? 4 : 0)),
+                        this.hasHideFlag(4), b -> this.addHideFlags(b ? 4 : 0)),
                 new PropertyBoolean("Hide 'Can destroy'",
-                        this.hasHideFlag(2), b -> this.addHideFlags(b ? 8 : 0)),
+                        this.hasHideFlag(3), b -> this.addHideFlags(b ? 8 : 0)),
                 new PropertyBoolean("Hide 'Can place on'",
-                        this.hasHideFlag(1), b -> this.addHideFlags(b ? 16 : 0)),
+                        this.hasHideFlag(2), b -> this.addHideFlags(b ? 16 : 0)),
                 new PropertyBoolean("Hide Potion effects & shield pattern info",
-                        this.hasHideFlag(0), b -> this.addHideFlags(b ? 32 : 0))
+                        this.hasHideFlag(1), b -> this.addHideFlags(b ? 32 : 0)),
+                new PropertyBoolean("Hide 'Dyed'",
+                        this.hasHideFlag(0), b -> this.addHideFlags(b ? 64 : 0))
         );
     }
 
@@ -33,7 +35,7 @@ public class HideFlagsCategory extends AbstractCategory {
     }
 
     private boolean hasHideFlag(int i) {
-        return String.format("%6s", Integer.toBinaryString(hideFlags)).replace(" ", "0").charAt(i) == '1';
+        return String.format("%7s", Integer.toBinaryString(hideFlags)).replace(" ", "0").charAt(i) == '1';
     }
 
     @Override
