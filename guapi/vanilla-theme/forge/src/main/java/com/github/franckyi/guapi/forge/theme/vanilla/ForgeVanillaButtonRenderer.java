ff6057f2e7e986f9ee23d96c8d97d9abddd7d30c
Franck Velasco <franck.velasco@hotmail.fr>
2021-08-27 13:39:10 +0200

    fix: play click sound on enum buttons when right-clicking (reverse cycle)

--- guapi/vanilla-theme/forge/src/main/java/com/github/franckyi/guapi/forge/theme/vanilla/ForgeVanillaButtonRenderer.java
+++ guapi/vanilla-theme/forge/src/main/java/com/github/franckyi/guapi/forge/theme/vanilla/ForgeVanillaButtonRenderer.java
@@ -1,12 +1,22 @@
 package com.github.franckyi.guapi.forge.theme.vanilla;
 
 import com.github.franckyi.guapi.api.node.Button;
+import com.github.franckyi.guapi.api.node.EnumButton;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraftforge.fml.client.gui.widget.ExtendedButton;
 
-public class ForgeVanillaButtonRenderer extends net.minecraft.client.gui.widget.button.Button implements ForgeVanillaDelegateRenderer {
+public class ForgeVanillaButtonRenderer extends ExtendedButton implements ForgeVanillaDelegateRenderer {
+    private final boolean isEnumButton;
+
     public ForgeVanillaButtonRenderer(Button node) {
         super(node.getX(), node.getY(), node.getWidth(), node.getHeight(), (ITextComponent) node.getLabel(), button -> {
         });
         initLabeled(node, this);
+        isEnumButton = node instanceof EnumButton;
+    }
+
+    @Override
+    protected boolean isValidClickButton(int button) {
+        return super.isValidClickButton(button) || (isEnumButton && button == 1);
     }
 }
