ff1d4c30b24a1046b22e23ad6e560847d8b7778c
Franck Velasco <franck.velasco@hotmail.fr>
2022-01-07 17:57:28 +0100

    fix: changed "default value" mechanism for text entries

--- common/src/main/java/com/github/franckyi/ibeeditor/base/client/mvc/model/entry/TextEntryModel.java
+++ common/src/main/java/com/github/franckyi/ibeeditor/base/client/mvc/model/entry/TextEntryModel.java
@@ -9,23 +9,16 @@ import java.util.function.Consumer;
 import static com.github.franckyi.guapi.api.GuapiHelper.*;
 
 public class TextEntryModel extends ValueEntryModel<TextComponent> {
-    private boolean initialized = false;
-
     public TextEntryModel(CategoryModel category, MutableComponent label, TextComponent value, Consumer<TextComponent> action) {
         super(category, label, value == null ? text() : value, action);
     }
 
     @Override
-    public void setValue(TextComponent value) {
-        if (!initialized) {
-            initialized = true;
-            defaultValue = value;
-        }
-        super.setValue(value);
-    }
-
-    @Override
     public Type getType() {
         return Type.TEXT;
+    }
+
+    public void resetDefaultValue() {
+        defaultValue = getValue();
     }
 }
