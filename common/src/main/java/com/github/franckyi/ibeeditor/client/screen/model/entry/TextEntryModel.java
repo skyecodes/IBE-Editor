8107d1ef830d480ec1e62ec9e69079d14dc667c2
Franck Velasco <franck.velasco@hotmail.fr>
2022-06-21 20:57:10 +0200

    fix: letter added at the end of a formatted text input is not formatted when saving

--- common/src/main/java/com/github/franckyi/ibeeditor/client/screen/model/entry/TextEntryModel.java
+++ common/src/main/java/com/github/franckyi/ibeeditor/client/screen/model/entry/TextEntryModel.java
@@ -8,8 +8,26 @@ import java.util.function.Consumer;
 import static com.github.franckyi.guapi.api.GuapiHelper.*;
 
 public class TextEntryModel extends ValueEntryModel<MutableComponent> {
+    private Runnable onApply;
+
     public TextEntryModel(CategoryModel category, MutableComponent label, MutableComponent value, Consumer<MutableComponent> action) {
         super(category, label, value == null ? text() : value, action);
+    }
+
+    public Runnable getOnApply() {
+        return onApply;
+    }
+
+    public void setOnApply(Runnable onApply) {
+        this.onApply = onApply;
+    }
+
+    @Override
+    public void apply() {
+        if (onApply != null) {
+            onApply.run();
+        }
+        super.apply();
     }
 
     @Override
